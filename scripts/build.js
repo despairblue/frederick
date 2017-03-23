d55934a3ffc28d383d28d8df2a9c9f5a74449caa
Danny Arnold <despair.blue@gmail.com>
2017-03-23 10:58:32 +0100

    fix build script when there is no build dir

--- scripts/build.js
+++ scripts/build.js
@@ -1,9 +1,11 @@
-const { exec, mkdir, mv, rm, set } = require('shelljs')
+const { exec, mkdir, mv, rm, set, test } = require('shelljs')
 
 set('-e')
 set('-v')
 
-rm('-R', 'build')
+if (test('-d', 'build')) {
+  rm('-R', 'build')
+}
 mkdir('build')
 
 exec('backpack build', { async: true })
